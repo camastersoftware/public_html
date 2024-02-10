906fe040508a802a0c2cecb56a49e500b3e3df5f
Priyanshu Singh <your.email@example.com>
2024-02-10 14:43:27 +0530

    CA

--- camastersoftware.com/app/Views/firm_panel/staff_administration/add_chartered_accountant.php
+++ camastersoftware.com/app/Views/firm_panel/staff_administration/add_chartered_accountant.php
@@ -274,7 +274,7 @@
                         ?>
                     </h4>
                     <div class="text-right flex-grow">
-                        <a href="<?= base_url('all-staff'); ?>">
+                        <a href="<?= base_url('staff-administration'); ?>">
                             <button type="button" class="waves-effect waves-light btn btn-sm btn-dark float-right">Back</button>
                         </a>
                     </div>
